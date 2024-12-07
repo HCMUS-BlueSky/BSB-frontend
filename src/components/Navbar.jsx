8f5c3fb9e8d019923eec894eded5961c1ad03ef3
Khoi Nguyen <nminhkhoi0818@gmail.com>
2024-12-07 22:08:48 +0700

    feat: add home page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Col, Container, Image, Row } from "react-bootstrap";
+
+const Navbar = () => {
+  return (
+    <div className="bg-white sticky p-4">
+      <Container>
+        <Row className="d-flex justify-content-between">
+          <Col>
+            <img src="" alt="BSB Logo" />
+          </Col>
+
+          <Col>
+            <div className="d-flex justify-content-end">
+              <div className="d-flex gap-2 align-items-center">
+                <h6>NGUYEN MINH KHOI</h6>
+                <Image src="/img/profile/default.svg" roundedCircle />
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,71 @@
 import React from "react";
+import Navbar from "../../components/Navbar";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Container>
+          <Row className="d-flex justify-content-center my-3">
+            <Col xs={10} md={6}>
+              <Button className="w-100" variant="orange">
+                <div className="d-flex justify-content-between p-2">
+                  <div className="text-light">
+                    <i className="bi bi-bell"></i>
+                    <span className="m-2">Lời nhắc của bạn</span>
+                  </div>
+                </div>
+              </Button>
+            </Col>
+          </Row>
+          <Row className="d-flex justify-content-center mb-3">
+            <Col xs={10} md={6}>
+              <Row>
+                <Col>
+                  <Button className="w-100" variant="light">
+                    <div className="d-flex justify-content-center p-2">
+                      <div className="d-flex gap-2 text-primary text-uppercase">
+                        <i className="bi bi-arrow-left-right"></i>
+                        <p>Chuyển tiền</p>
+                      </div>
+                    </div>
+                  </Button>
+                </Col>
+                <Col>
+                  <Button className="w-100" variant="light">
+                    <div className="d-flex justify-content-center p-2">
+                      <div className="d-flex gap-2 text-primary text-uppercase">
+                        <i className="bi bi-cash-coin"></i>
+                        <p className="text-primary text-uppercase">Nhắc nợ</p>
+                      </div>
+                    </div>
+                  </Button>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          <Row className="d-flex justify-content-center mb-3">
+            <Col xs={10} md={6}>
+              <Button className="w-100" variant="light">
+                <div className="d-flex justify-content-between align-items-center p-2">
+                  <div className="d-flex flex-column gap-2">
+                    <p className="text-start">Tài khoản chính</p>
+                    <div>
+                      <i className="bi bi-bank"></i>
+                      <span className="m-2">Số tài khoản: 902127855814</span>
+                    </div>
+                  </div>
+                  <p className="text-primary fs-4">0 vnd</p>
+                </div>
+              </Button>
+            </Col>
+          </Row>
+        </Container>
+      </main>
+    </>
+  );
 };
 
 export default Home;
