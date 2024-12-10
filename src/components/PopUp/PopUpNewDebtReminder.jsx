481f5bdf319f12801f3251fe5a5e2779b441a6f9
kietnguyen2003 <nguyengiakiet0987654321@gmail.com>
2024-12-10 15:42:46 +0700

    feat: update PopUpNewDebtReminder component

--- src/components/PopUp/PopUpNewDebtReminder.jsx
+++ src/components/PopUp/PopUpNewDebtReminder.jsx
@@ -6,12 +6,21 @@ import DropdownBank from "../Dropdown/DropdownBank";
 const PopUpNewDebtReminder = ({ show, handleClose }) => {
 
   const [bank, setBank] = useState([]);
+  const [selectAccount, setSelectAccount] = useState("BẠN MUỐN NHẮC NỢ TỚI AI?");
+  const [amount, setAmount] = useState("");
+  const [reason, setReason] = useState("");
+
+  const handleSubmit = () => {
+    console.log("Bank: ", selectAccount);
+    console.log("Amount: ", amount);
+    console.log("Reason: ", reason);
+  };
 
   useEffect(() => {
     const data = [
-      { id: 1, name: "KHOI", icon:"", descripton: "LEADER" },
-      { id: 2, name: "UYEN", icon:"", descripton: "KHUNG" },
-      { id: 3, name: "NHAN", icon:"", descripton: "TRAM" },
+      { id: 1, name: "KHOI", icon:"", description: "LEADER" },
+      { id: 2, name: "UYEN", icon:"", description: "KHUNG" },
+      { id: 3, name: "NHAN", icon:"", description: "TRAM" },
     ];
     setBank(data);
   }, []);
@@ -60,7 +69,7 @@ const PopUpNewDebtReminder = ({ show, handleClose }) => {
 
         <Row className="d-flex justify-content-center align-items-center mb-4">
           <Col xs={12} md={10}>
-            <DropdownBank bank={bank}/>        
+            <DropdownBank bank={bank} selectedBank={selectAccount} setSelectedBank={setSelectAccount}/>        
           </Col>
         </Row>
 
@@ -71,7 +80,12 @@ const PopUpNewDebtReminder = ({ show, handleClose }) => {
               controlId="txtAccountNumber" 
               label="SỐ TIỀN"
             >
-              <Form.Control placeholder="123456789" className="w-100" />
+              <Form.Control 
+                placeholder="123456789" 
+                className="w-100" 
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+              />
             </FloatingLabel>
           </Col>
         </Row>
@@ -87,7 +101,9 @@ const PopUpNewDebtReminder = ({ show, handleClose }) => {
                 as="textarea"
                 rows={3}
                 placeholder="" 
-                className="w-100" 
+                className="w-100"
+                value={reason}
+                onChange={(e) => setReason(e.target.value)} 
               />
             </FloatingLabel>
           </Col>
@@ -99,8 +115,9 @@ const PopUpNewDebtReminder = ({ show, handleClose }) => {
         <Col xs={12} md={10}>
           <Button 
             variant="primary" 
-            onClick={handleClose} 
+            // onClick={handleClose} 
             className="w-100 p-2"
+            onClick={handleSubmit}
           >
             <div className="d-flex justify-content-center align-items-center">
               <span className="text-light fw-bold">Tìm người nhận</span>
