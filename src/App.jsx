9b2cef203eac56317bd41b23ab06ccf95c86d06f
tmhuyen <21127202@student.hcmus.edu.vn>
2024-12-11 18:15:36 +0700

    feat: UI search receiver and add receiver

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
+import TransferMoney from "./pages/TransferMoney/TransferMoney";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/transfer-money" element={<TransferMoney />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/utils/formatCurrency.js
@@ -0,0 +1,3 @@
+export const formatCurrency = (amount) => {
+    return new Intl.NumberFormat("de-DE").format(amount);
+  };
