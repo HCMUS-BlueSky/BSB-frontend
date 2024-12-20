ab74681b1c36dbe79ea813d9e29bfc5e83053005
tmhuyen <21127202@student.hcmus.edu.vn>
2024-12-20 20:04:27 +0700

    chore: update logo and add action redirect to the homapage when click in the logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import { Col, Container, Image, Row } from "react-bootstrap";
 import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const { user } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate("/");
+  };
 
   return (
     <div className="bg-white sticky p-4">
       <Container>
         <Row className="d-flex justify-content-between">
           <Col>
+            {/* Add onClick event to navigate to the home page */}
             <img
-              style={{ width: "50px", height: "50px" }}
+              style={{ width: "50px", height: "50px", cursor: "pointer" }}
               src="/img/logo/logo.png"
               alt="BSB Logo"
+              onClick={handleLogoClick}
             />
           </Col>
 
